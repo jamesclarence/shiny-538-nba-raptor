1404ee410befa04c2e00dbdfcba7c4e953788a9d
James Fisher <fisher.jc@gmail.com>
2022-02-27 15:48:56 -0500

    add ggplot scatter chart

--- app.R
+++ app.R
@@ -54,6 +54,7 @@ ui <- fluidPage(
       p("WAR: Wins Above Replacement. How many wins a player will contribute compared to a replacement-level player, which 538 determines to be -2.75 points per 100 possessions.")
   ),
     mainPanel(
+      plotOutput("plot"),
       gt_output(outputId = "table") # GT Table
     )
   )
@@ -112,25 +113,26 @@ server <- function(input, output) {
           align = "center"
         )
     )
+  
+  output$plot <- 
+    renderPlot(
+      filtered() %>% 
+        ggplot() +
+        geom_rect(aes(
+          xmin = 0, xmax = 10, ymin = 0, ymax = 10),
+          fill = "lightblue"
+        ) +
+        geom_rect(aes(
+          xmin = 0, xmax = -10, ymin = 0, ymax = -10),
+          fill = "pink"
+        ) +
+        geom_point(aes(x = raptor_offense,
+                       y = raptor_defense),
+                   shape = 21, colour = "black", fill = "white", size = 5, stroke = 2) +
+        xlim(-10, 10) +
+        ylim(-10, 10)
+    )
 }
 
 # Run the app ----
 shinyApp(ui = ui, server = server)
-
-# Chart
-# current_raptor %>%
-#   filter(mp > 829) %>% 
-#   ggplot() + 
-#   geom_rect(aes(
-#     xmin = 0, xmax = 10, ymin = 0, ymax = 10),
-#     fill = "lightblue"
-#   ) +  
-#   geom_rect(aes(
-#     xmin = 0, xmax = -10, ymin = 0, ymax = -10),
-#     fill = "pink"
-#   ) +
-#   geom_point(aes(x = raptor_offense,
-#                  y = raptor_defense),
-#              shape = 21, colour = "black", fill = "white", size = 5, stroke = 2) +
-#   xlim(-10, 10) +
-#   ylim(-10, 10)
